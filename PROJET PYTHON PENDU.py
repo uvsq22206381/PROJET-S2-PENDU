3518dafa3d841c80741df10a1f1e74db8ecc7247
Carl KEYROUZ <carlkeyrouz2@gmail.com>
2023-04-16 15:34:18 +0200

    VERSION NON FINALE DU PROJET --> tous les commit qui seront établis à partir de celui-ci risquent d'être abandonnés si on n'a pas le temps de terminer ajout de 3 modes, le mode infini, chrono, et normal chaque mode possède des instructions qui lui ont associées, celles si sont stockées das le fichier instructions. réorganisation de toute l'interface du menu principal, ajout des modes et de leurs instructions relatives, il y a cependant encore du changement à faire changement de la fonction de positionnement dans le frame jeu de pack à place car ce dernier est plus simple à organiser beaucoup de changements au niveau du 'design' sont encore à venir

--- PROJET PYTHON PENDU.py
+++ PROJET PYTHON PENDU.py
@@ -9,7 +9,7 @@ themes   =    {'Animaux': ['chien', 'chat', 'lion', 'tigre', 'éléphant'],
                'Fruits': ['pomme', 'banane', 'orange', 'kiwi', 'mangue'],
                'Couleurs': ['rouge', 'bleu', 'vert', 'jaune', 'violet']}
 
-dict_accents =  {
+dict_accents =  {           #dictionnaire des lettres avec accents les plus courantes en francais 
         'à': 'a',
         'â': 'a',
         'ä': 'a',
@@ -26,10 +26,7 @@ dict_accents =  {
         'û': 'u',
         'ü': 'u',
     }
-
-
-#création de l'interface du jeu
-
+#Création et caractérisation de la racine de l'inte
 racine = tk.Tk()
 racine.title("PENDU")
 racine.attributes('-fullscreen', True)
@@ -37,79 +34,185 @@ racine.update()
 WIDTH = racine.winfo_width()
 HEIGHT = racine.winfo_height()
 racine.config(bg = "#282828")
-
+menu = tk.Frame(racine, bg="#282828")       #écran du menu principal
 
 def menu_principal():      #création de l'écran principal du jeu
-    global menu
-    menu = tk.Frame(racine, bg="#282828")
+    global texte_instructions, theme_et_long
     menu.pack(pady = 30)
-    title = tk.Label(menu, text='le Jeu du Pendu', font = 'jokerman 60 bold italic', bg = "#282828")
-    title.pack(pady = 20)
+    title = tk.Label(menu, text='le Jeu du Pendu', font = 'jokerman 60 bold italic', bg = "#F3E5AB")
+    title.pack(pady = 20, ipadx = WIDTH/2)
+    lire_les_regles()
+    texte_instructions = tk.Label(menu, width = WIDTH, height = 6, bd =4, relief = 'raised', bg = 'black', text = texte, font= 'Monolisa, 10', fg = 'red')
+    texte_instructions.pack(pady = 5, ipady = 5)
+    choix_mode()
+    theme_et_long = tk.Frame(menu, width = WIDTH, height = 150, bg = "#282828")
+    theme_et_long.pack(pady = 50)
+    jouer_quitter()
+
+def lire_les_regles():          #fonction permettant de lire et d'afficher les règles du jeu 
+    global texte
+    règles = open('C:/PROJET PYTHON PENDU/PROJET-S2-PENDU/instructions.txt', 'r',  encoding='utf-8')
+    ligne = règles.readline()
+    texte = ''
+    while (ligne.strip() != ''):
+        texte += ligne 
+        ligne = règles.readline()
+    règles.close()
+    return texte
+
+def choix_mode():           #toute l'interface concernant les modes dans le menu
+    global modes
+    bande_de_modes = tk.Frame(menu, width = WIDTH, height = 100, bg = "#282828") 
+    bande_de_modes.pack(pady = 10)
+    choisir_mode = tk.Label(bande_de_modes, text = 'Choisir un mode :', font = 'times 30 italic', fg = 'white', bg = "#282828")
+    choisir_mode.place(anchor = 'w', relx = 0.03, rely = 0.5)
+    mode_normal = tk.Button(bande_de_modes, text = 'Normal', font = 'helvetica 20 bold', bg = "#F3E5AB",  command = lambda: instructions_mode(mode_normal))
+    mode_normal.place(relx = 0.4, rely = 0.5, anchor= 'center')
+    mode_chrono = tk.Button(bande_de_modes, text = 'Chrono', font = 'helvetica 20 bold', bg = "#F3E5AB", command = lambda: instructions_mode(mode_chrono))
+    mode_chrono.place(relx = 0.6, rely = 0.5, anchor= 'center')
+    mode_infini = tk.Button(bande_de_modes, text = 'Infini', font = 'helvetica 20 bold', bg = "#F3E5AB", command = lambda: instructions_mode(mode_infini))
+    mode_infini.place(relx = 0.8, rely = 0.5, anchor= 'center')
+    modes = [mode_chrono, mode_infini, mode_normal]
+
+def chrono():               #création du chronomètre
+    global temps, texte_tps
+    temps = 120
+    texte_tps = tk.StringVar()
+    texte_tps.set("02:00")
+    cercle = tk.Canvas(jeu, width=280, height=280, bg="#0A1931", bd=0, highlightthickness=0)
+    cercle.place(relx = 0.2, rely = 0.3, anchor= 'center')
+    cercle.create_oval((10, 10),(270, 270), outline="#F5A623", width=10)
+    tps = tk.Label(cercle, textvariable=texte_tps, font=("Arial", 80, "bold"), bg="#0A1931", fg="#F5A623")
+    tps.place(relx=0.5, rely=0.5, anchor="center")
+
+def passage_temps():        #passage du temps au chrono
+    temps -= 1
+    minutes, seconds = temps//60, temps%60
+    texte_tps.set(f"{minutes:02d}:{seconds:02d}")
+    if temps == 0:
+        pass
+    else:
+        racine.after(1000, passage_temps)
+
+def jouer_quitter():       #toute l'interface concernant les boutons jouer et quitter dans le menu
+    play_exit = tk.Frame(menu , width = WIDTH, height = 100, bg = "#282828")
+    play_exit.pack(pady = 5)
+    play_button = tk.Button(play_exit, text="Commencer", font=("Helvetica", 20, "bold"), fg="white", bg="#4CAF50", command = transition)
+    play_button.place(relx = 0.7, rely = 0.5, anchor = 'center')
+    exit_button = tk.Button(play_exit, text="Quitter", font=("Helvetica", 20, "bold"), fg="white", bg="#F44336", command = racine.destroy)
+    exit_button.place(relx = 0.3, rely = 0.5, anchor = 'center') 
+
+def instructions_mode(button):      #fonction qui permet de lire et d'afficher les instructions en relation avec chaque mode
+  global instr_mode, mode
+  button.config(relief = 'sunken')
+  for boutton in modes:
+    if boutton != button:
+      boutton.config(relief = 'raised')
+      for widget in theme_et_long.winfo_children():
+        widget.place_forget()
+  mode = button['text']
+  instr_mode = ''
+  règles = open('C:/PROJET PYTHON PENDU/PROJET-S2-PENDU/instructions.txt', 'r',  encoding='utf-8')
+  lignes = règles.readline()
+  while lignes.strip() != mode:
+    lignes = règles.readline()
+  lignes = règles.readline()
+  while lignes.strip() != '':
+    instr_mode += lignes
+    lignes = règles.readline()
+  règles.close()
+  paramètres_modes()
+  texte_instructions.config(text = instr_mode)
+
+def affich_combo():
+  choisir_theme.place(relx = 0.3, rely = 0.1, anchor = 'center')
+  combo_themes.place(relx= 0.3, rely =0.4, anchor = 'center')
+  choisir_long_mot.place(relx = 0.7, rely = 0.1, anchor= 'center')
+  combo_long.place(relx = 0.7, rely =0.4, anchor='center')
+
+def liste_de_themes():      #fonction qui permet à l'utilisateur de choisir le thème des mots à deviner
+  global combo_themes, choisir_theme
+  choisir_theme = tk.Label(theme_et_long, text = 'Choisir un thème de mot:', font = 'helvetica 20 italic', fg = 'white', bg ="#282828")
+  theme_options = tk.StringVar()
+  combo_themes = ttk.Combobox(theme_et_long, textvariable= theme_options, width = 20, state="readonly")                    
+  combo_themes['values'] = list(themes.keys())
+  combo_themes.bind("<<ComboboxSelected>>",  sélection_thème)
+
+def extraction_des_longueurs():       #fonction qui permet d'extraire toutes les longueurs de mots possibles pour un thème donné
+  global longueur_des_mots   
+  longueur_des_mots = []                      
+  mots_du_theme = themes[thème]
+  for i in mots_du_theme:
+    longueur_des_mots.append(len(i))
+  longueur_des_mots = list(set(longueur_des_mots))
+  longueur_des_mots.sort()
+    
+def liste_de_longueurs():       #fonction qui permet à l'utilisateur de choisir la longueur du mot à deviner
+  global combo_long, choisir_long_mot
+  choisir_long_mot = tk.Label(theme_et_long, text = 'Choisir la longueur du mot:', font = 'helvetica 20 italic', fg = 'white', bg ="#282828")    
+  combo_long = ttk.Combobox(theme_et_long, width = 20, state="readonly")
+  combo_long.bind("<<ComboboxSelected>>",  sélection_long)
+
+def sélection_thème(event):     ##fonction qui permet de retourner le thème de mots choisi par l'utilisateur
+  global thème
+  thème = combo_themes.get()
+  extraction_des_longueurs()
+  combo_long['values'] = list(longueur_des_mots)
+  combo_long.current(0)
+    
+def sélection_long(event):       #fonction qui permet de retourner la longueur du mot choisi par l'utilisateur
+  global longueur  
+  longueur = combo_long.get()
+
+def affich_combo_theme():
+    choisir_theme.place(relx = 0.5, rely = 0.1, anchor = 'center')
+    combo_themes.place(relx= 0.5, rely =0.4, anchor = 'center')
+
+def paramètres_modes():
     liste_de_themes()
     combo_themes.current(0)
     liste_de_longueurs()
-    play_button = tk.Button(menu, text="Commencer", font=("Helvetica", 20, "bold"), fg="white", bg="#4CAF50", command = transition)
-    play_button.pack(pady=20)           #création du bouton pour commencer la partie
-    exit_button = tk.Button(menu, text="Quitter", font=("Helvetica", 20, "bold"), fg="white", bg="#F44336", command = racine.destroy)
-    exit_button.pack(pady=20)           #création du bouton de sortie du jeu
-    
-def liste_de_themes():      #fonction qui permet à l'utilisateur de choisir le thème des mots à deviner
-    global combo_themes
-    choisir_theme = tk.Label(menu, text = 'Choisir un thème de mot:', font = 'helvetica 20 italic', fg = 'white', bg ="#282828")
-    choisir_theme.pack(pady = 10)
-    theme_options = tk.StringVar()
-    combo_themes = ttk.Combobox(menu, textvariable= theme_options, width = 20, state="readonly")                    
-    combo_themes['values'] = list(themes.keys())
-    combo_themes.pack(pady = 10)
-    combo_themes.bind("<<ComboboxSelected>>",  sélection_thème)
+    if mode == 'Normal':
+        affich_combo()
+    elif mode == 'Chrono':
+        affich_combo_theme()
+    else:
+        affich_combo_theme()
 
-def extraction_des_longueurs():       #fonction qui permet d'extraire toutes les longueurs de mots possibles pour un thème donné
-    global longueur_des_mots   
-    longueur_des_mots = []                      
-    mots_du_theme = themes[thème]
-    for i in mots_du_theme:
-        longueur_des_mots.append(len(i))
-    longueur_des_mots = list(set(longueur_des_mots))
-    longueur_des_mots.sort()
-    
-def liste_de_longueurs():       #fonction qui permet à l'utilisateur de choisir la longueur du mot à deviner
-    global combo_long
-    choisir_long_mot = tk.Label(menu, text = 'Choisir la longueur du mot:', font = 'helvetica 20 italic', fg = 'white', bg ="#282828")
-    choisir_long_mot.pack(pady = 10)
-    combo_long = ttk.Combobox(menu, width = 20, state="readonly")
-    combo_long.pack(pady = 10)
-    combo_long.bind("<<ComboboxSelected>>",  sélection_long)
+def particularités_mot():
+    if mode == 'Normal':
+        mot_caché_Normal()
+    else:
+        mot_caché_Autres()            
 
-def sélection_thème(event):     ##fonction qui permet de retourner le thème de mots choisi par l'utilisateur
-    global thème
-    thème = combo_themes.get()
-    extraction_des_longueurs()
-    combo_long['values'] = list(longueur_des_mots)
-    combo_long.current(0)
-    
-def sélection_long(event):       #fonction qui permet de retourner la longueur du mot choisi par l'utilisateur
-    global longueur  
-    longueur = combo_long.get()    
+#création de l'interface du jeu
     
 def transition():       #transition vers le jeu principal
     global jeu
     menu.destroy()
-    jeu = tk.Frame(racine, bg="#282828")
-    jeu.pack(pady=30)
+    jeu = tk.Frame(racine, bg="#282828", width = WIDTH, height = HEIGHT)
+    jeu.pack()
+    particularités_mot()
     cadre_pendu()
-    mot_caché()
     dessin_mot()
     clavier_lettres()
     nouvelle_partie()
+    particularités_mode()
     jeu.focus_set()
     jeu.bind("<KeyPress>", clavier_ordi)
+    
         
+def particularités_mode():
+    if mode == 'Chrono':
+        chrono()
+
+
 def nouvelle_partie():          #permet de générer une nouvelle partie
     global guesses, wrong_guesses, right_guesses
     dessin_pendu.delete('all')
     wrong_guesses= 0
     guesses = 0
-    mot_caché()
+    particularités_mot()
     right_guesses = list(set(mot_séparé_normal))
     List_dash.config(text = ' '.join(["_" if letter!= ' ' else ' ' for letter in Mot]))
     for button in buttons:
@@ -118,9 +221,9 @@ def nouvelle_partie():          #permet de générer une nouvelle partie
 def cadre_pendu():          #crée le cadre dans lequel se formera le pendu
     global dessin_pendu
     dessin_pendu = tk.Canvas(jeu, bg ='white', width = 400, height = 400)
-    dessin_pendu.pack(side ="top")
+    dessin_pendu.place(relx = 0.5, rely = 0.3, anchor = 'center')
 
-def mot_caché():        #génère le mot aléatoire qu'il faudra deviner
+def mot_caché_Normal():        #génère le mot aléatoire qu'il faudra deviner
     global Mot, Mot_séparé
     Mot = ''
     while len(Mot) != int(longueur):
@@ -128,7 +231,14 @@ def mot_caché():        #génère le mot aléatoire qu'il faudra deviner
     Mot_séparé = [x.upper() for x in Mot]
     normalisation_mot()
 
-def normalisation_mot():
+def mot_caché_Autres():
+    global Mot, Mot_séparé
+    Mot = ''    
+    Mot = rd.choice(themes[thème])
+    Mot_séparé = [x.upper() for x in Mot]
+    normalisation_mot()
+
+def normalisation_mot():                #fonction qui permet de retirer tous les accents d'un mot pour faciliter la reconnaissance
     global mot_séparé_normal
     mot_séparé_normal = []
     for x in Mot_séparé:
@@ -140,14 +250,14 @@ def normalisation_mot():
 def dessin_mot():                   #Dessine les traits du mots qu'il faudra deviner
     global List_dash
     dessin = tk.Frame(jeu, bg = "LightGoldenrod2", width = 550, height = 100, highlightbackground= 'midnightblue', bd= 10, relief = 'raised')
-    dessin.pack(pady=30, anchor = 'center')
+    dessin.place(relx = 0.5, rely = 0.67, anchor = 'center')
     List_dash = tk.Label(jeu, text = ' '.join(["_" if letter!= ' ' else ' ' for letter in Mot]), font = ('helvetica', 50), fg = 'black', bg ='LightGoldenrod2')
     List_dash.place(in_=dessin, relx =0.5, rely = 0.5, anchor = 'center')
 
 def clavier_lettres():          #construit un clavier contenant des boutons pour chacune des 26 lettres de l'alphabet 
     global alphabet, buttons
     clavier = tk.Frame(jeu, bg="#282828", bd = 2, relief = 'sunken')
-    clavier.pack(pady = 0, anchor = 'center')
+    clavier.place(relx = 0.5, rely = 0.88, anchor = 'center')
     alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
     buttons = []
     for letter in alphabet:
@@ -245,4 +355,4 @@ def victoire():                         #Fonction qui affiche un message de vict
 
 menu_principal()
 racine.mainloop()
-print(mot_séparé_normal)
+
